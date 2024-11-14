3781571a461e1743422f0eae7ff58da00f133df9
jtobal <ziba.tobal@gmail.com>
2024-11-13 21:38:27 -0300

    se crea un metodo para leer los txt y convertirlos en JSON. Se comprueba su funcionamiento

--- metodos_temp.ts
+++ metodos_temp.ts
@@ -1,63 +1,28 @@
-import * as fs from 'fs'
-
-const proveedoresBBDD = fs.readdirSync('./bbdd/proveedores.txt', "utf8");
-
-/*export default class GestorDeArchivos {
-
-    private arregloString: string[];
-
-    constructor(txtFileLocation: string) {
-        
-        let archivoTxt: string = fs.readFileSync(txtFileLocation, 'utf-8'); 
-        this.arregloString = archivoTxt.split(';');  
-    
-    }
-
-    public mostrarArreglo(): void {
-        console.log(this.arregloString);
-    }
-
-    public getArregloString(): string[] {
-        return this.arregloString;
-    }
-
-}
- */
-
-
-
-
-/*
 import * as fs from 'fs';
 
-const bbdd= fs.readFileSync('./bbdd.txt', "utf8");
+const proveedores = leerTXT('./bbdd/proveedores.txt');
 
-console.log(bbdd)
+//FUNCION PARA LEER CUALQUIER TXT Y CONVERTIRLOS A JSON//
+function leerTXT(ruta: string): any[] {
+  try {
+    const archivoTexto = fs.readFileSync(ruta, 'utf8');    
+    const lineas = archivoTexto.trim().split('\n');
+    
+    const proveedoresJSON = lineas.map(linea => {
+      const [id, nombre, telefono] = linea.split(',');
+      return {
+        ID: id.trim(),
+        Nombre: nombre.trim(),
+        Telefono: telefono.trim()
+      };
+    });
+    
+    return proveedoresJSON; 
+  } catch (error) {
+    console.error("Error al leer el archivo:", error); //Control de errores.
+    return [];
+  }
+}
 
-const datosProductos: string= fs.readFileSync('./bbdd.txt', 'utf-8');
-const productosSinEspacios: string = datosProductos.trim();
-const productosArray: string[] = productosSinEspacios.split("|"); 
-console.log(productosArray);
-
-const productosJSON: any[] =[];
-
-for (let i:number=0; i < productosArray.length; i++){
-    const productosPorLinea: string[] = productosArray[i].split(",");
-    const nombre = productosPorLinea[0];
-    const precio = productosPorLinea[1];
-    const stock = productosPorLinea[2];
-    const categoria = productosPorLinea[3]
-
-    const producto={
-        nombre: nombre,
-        precio: precio,
-        stock: stock,
-        categoria: categoria
-    }
-
-    productosJSON.push(producto);
-
-    }
-const pepito=JSON.stringify(productosJSON, null, 2);
-
-console.log(pepito); */
+console.log(proveedores); //Impresion de consola para pruebas
+console.log(proveedores[1].Nombre);  //Verificacion - Debug
